94f0095f208e51ec80c45572738a2797bac9d53e
Goma Nanoha <goma@goma-gz.com>
2015-10-23 19:25:44 +0900

    Revert "ListsControllerの書き方を一新"

--- app/controllers/ListsController.php
+++ app/controllers/ListsController.php
@@ -1,115 +1,123 @@
 <?php
-/**
- *  [API]リストに関するクラス
- *
- *  リストを操作に関するAPIをまとめたコントローラー
- *  エンドポイント単位でメゾットを定義
- *  Privateメゾットはプリフィックスにアンダーバー[_]をつける
- *
- *  @access public
- *  @author Goma::NanoHa <goma@goma-gz.net>
- *  @extends ControllerAPI
- */
 
 class ListsController extends ControllerAPI
 {
 
-    /**
-     *  [POST]リストの登録メゾット
-     *
-     *  Endpoint POST /apo/addList
-     *
-     *  @access public
-     *  @return JSON Responce
-     */
     public function addAction()
     {
 
+        /**
+         *  POST受け取り
+         */
         $dashboard_id = $this->request->getPost('dashboard', null, null);
         $list = $this->request->getPost('list', null, null);
 
-        if(empty($list)) {
-            $this->_status['status'] = false;
-            $this->_status['error'] = 'Argument is not enough';
-        }
+        if(!empty($list)) {
 
-        $templateList = [
-            'title' => null,
-            'tags_id' => null,
-            'comment' => null,
-            'url' => null
-        ];
-        $conditions = [
-            'title',
-            'url'
-        ];
-        if($this->_status['status'] && !$this->_mergeArray($list, $templateList, $conditions)) {
-            $this->_status['status'] = false;
-            $this->_status['error'] = 'JSON Argument is not enough';
-        }
-
-        if($this->_status['status'] && !$this->_checkToken()) {
-            $this->_status['status'] = false;
-            $this->_status['error'] = 'Login Failed';
-        }
-
-        if($this->_status['status'] && !$this->_isURL($list['url'])) {
-            $this->_status['status'] = false;
-            $this->_status['error'] = 'URL is Not';
-        }
-
-        if(!$this->_status['status']) {
-            return $this->response->setJsonContent($this->_status);
-        }
-
-        if(empty($dashboard_id)) {
-
-            $dashboard = Dashboard::findFirst(
-                [
-                    'users_id = ?1 AND default = ?2',
-                    'bind' => [
-                        1 => $this->_id,
-                        2 => true
-                    ]
-                ]
+            /**
+             *  リストチェック
+             */
+            $templateList = array(
+                'title' => null,
+                'tags_id' => null,
+                'comment' => null,
+                'url' => null
+            );
+            $conditions = array(
+                'title',
+                'url'
             );
 
-            $dashboard_id = $dashboard->id;
+            if($this->_mergeArray($list, $templateList, $conditions)) {
 
-        } else {
+                if($this->_checkToken()) {
 
-            $dashboard = Dashboard::findFirst(
-                [
-                    'users_id = ?1 AND id = ?2',
-                    'bind' => [
-                        1 => $this->_id,
-                        2 => $dashboard_id
-                    ]
-                ]
-            );
+                    if($this->_isURL($list['url'])) {
 
-            if(empty($dashboard)) {
-                $this->_status['status'] = false;
-                $this->_status['error'] = 'DashboardID can not be found';
-                return $this->response->setJsonContent($this->_status);
+                        if(empty($dashboard_id)) {
+
+                            $dashboard = Dashboard::findFirst(
+                                array(
+                                    'users_id = ?1 AND default = ?2',
+                                    'bind' => array(
+                                        1 => $this->_id,
+                                        2 => true
+                                    )
+                                )
+                            );
+
+                            $dashboard_id = $dashboard->id;
+
+                        } else {
+
+                            $dashboard = Dashboard::findFirst(
+                                array(
+                                    'users_id = ?1 AND id = ?2',
+                                    'bind' => array(
+                                        1 => $this->_id,
+                                        2 => $dashboard_id
+                                    )
+                                )
+                            );
+
+                            if(!empty($dashboard)) {
+                                $dashboard_id = $dashbord->id;
+                            } else {
+                                $this->_status = array(
+                                    'status' => false,
+                                    'error' => 'DashboardID can not be found'
+                                );
+                            }
+                        }
+
+                        $urls = new Urls();
+                        $urls->assign(
+                            array(
+                                'dashboard_id' => $dashboard_id,
+                                'tags_id' => $list['tags_id'],
+                                'title' => $list['title'],
+                                'comment' => $list['comment'],
+                                'url' => $list['url']
+                            )
+                        );
+
+                        if($urls->save()) {
+                            $this->_status = array(
+                                'status' => true
+                            );
+                        } else {
+                            $this->_status = array(
+                                'status' => false,
+                                'error' => 'Unknow Error'
+                            );
+                        }
+
+                    } else {
+                        $this->_status = array(
+                            'status' => false,
+                            'error' => 'URL is Not'
+                        );
+                    }
+
+                } else {
+                    $this->_status = array(
+                        'status' => false,
+                        'error' => 'Login failed'
+                    );
+                }
+
+            } else {
+                $this->_status = array(
+                    'status' => false,
+                    'error' => 'JSON Argument is not enough'
+                );
             }
 
-        }
-
-        $urls = new Urls();
-        $urls->assign(
-            [
-                'dashboard_id' => $dashboard_id,
-                'tags_id' => $list['tags_id'],
-                'title' => $list['title'],
-                'comment' => $list['comment'],
-                'url' => $list['url']
-            ]
-        );
-
-        if(!$urls->save()) {
-            $this->_status['status'] = false;
-            $this->_status['error'] = 'Unknow Error';
+        } else {
+            $this->_status = array(
+                'status' => false,
+                'error' => 'Argument is not enough'
+            );
         }
 
         return $this->response->setJsonContent($this->_status);
@@ -120,12 +128,9 @@ class ListsController extends ControllerAPI
      *  Listのマージ及び必要項目のNullチェック
      *
      *  @access private
-     *  @param JSON &$list
-     *      リスト
-     *  @param array $templateList
-     *      リストのテンプレート
-     *  @param array $conditions
-     *      リストの必要項目
+     *  @param (Json:使用後Array) &$list
+     *  @param (array) $templateList
+     *  @param (array) $conditions
      *  @return boolean
      */
     private function _mergeArray(&$list, $templateList, $conditions)
@@ -134,9 +139,7 @@ class ListsController extends ControllerAPI
         $list = array_merge($templateList, $json);
         $status = true;
         foreach($conditions as $key) {
-            if($status) {
-                $status = !empty($list[$key]);
-            }
+            $status = !empty($list[$key]);
         }
         return $status;
     }
@@ -145,8 +148,7 @@ class ListsController extends ControllerAPI
      *  URL チェック
      *
      *  @access private
-     *  @param string $url
-     *      URL
+     *  @param (string:URL) $url
      *  @return boolean
      */
     private function _isURL($url)
@@ -155,4 +157,3 @@ class ListsController extends ControllerAPI
     }
 
 }
-
