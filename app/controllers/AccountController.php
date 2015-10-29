5806a47a71c3a0e802cd97ae08e7237e40b77f80
gomasiyo <goma@goma-gz.com>
2015-10-30 01:55:56 +0900

    Accountを新仕様に

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -1,63 +1,72 @@
 <?php
+/**
+ *  [API]アカウントに関するクラス
+ *
+ *  アカウントに関するAPIをまとめたコントローラー
+ *  エンドポイント単位でメゾットを定義
+ *  Privateメゾットはプリフィックスにアンダーバー[_]をつける
+ *
+ *  @access public
+ *  @author Goma::NanoHa <goma@goma-gz.net>
+ *  @extends ControllerAPI
+ */
 
 class AccountController extends ControllerAPI
 {
 
+    /**
+     *  [POST]ログインメゾット
+     *
+     *  Endpoint POST /api/account/login
+     *
+     *  @access public
+     *  @return JSON Responce
+     */
     public function loginAction()
     {
 
-        /**
-         * POST受け取り
-         */
-        $name = $this->request->getPost('name');
-        $passwd = $this->request->getPost('passwd');
-
-        if(!(empty($name) || empty($passwd))) {
-
-            $user = Users::findFirst(
-                array(
-                    'name = ?1 OR email = ?1',
-                    'bind' => array(
-                        1 => $name
-                    )
-                )
-            );
-
-            if($user) {
-                if($this->security->checkHash($passwd, $user->password)) {
-
-                    /**
-                     *  Token作成 hash(id+name)
-                     */
-                    $token = $this->security->hash($user->id . '+' . $user->name);
-
-                    $this->_status = array(
-                        'status' => true,
-                        'id' => $user->id,
-                        'name' => $user->name,
-                        'screen_name' => $user->screen_name,
-                        'token' => $token
-                    );
-
-                } else {
-                    $this->_status = array(
-                        'status' => false,
-                        'error' => 'Login failed'
-                    );
-                }
-            } else {
-                $this->_status = array(
-                    'status' => false,
-                    'errer' => 'Login failed'
-                );
-            }
-
-        } else {
-            $this->_status = array(
-                'status' => false,
-                'error' => 'Argument is not enough'
-            );
+        $post = [
+            'name' => true,
+            'passwd' => true
+        ];
+        if($this->_getPost($post)) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 201;
+            $this->_status['response']['detail'] = $post['empty'];
         }
+
+        if(!$this->_status['response']['status']) {
+            return $this->response->setJsonContent($this->_status);
+        }
+
+
+        $user = Users::findFirst(
+            array(
+                'name = ?1 OR email = ?1',
+                'bind' => array(
+                    1 => $this->_post['name']
+                )
+            )
+        );
+
+        if(!$user || !$this->security->checkHash($this->_post['passwd'], $user->password)) {
+            $this->_status['response']['status'] = false;
+            $this->_status['response']['code'] = 302;
+            return $this->response->setJsonContent($this->_status);
+        }
+
+
+        $token = $this->security->hash($user->id . '+' . $user->name);
+
+        $result = array(
+            'status' => true,
+            'id' => $user->id,
+            'name' => $user->name,
+            'screen_name' => $user->screen_name,
+            'token' => $token
+        );
+
+        $this->_status['result'] = $result;
 
         return $this->response->setJsonContent($this->_status);
 
